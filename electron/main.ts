01293bc8c11d0f8708dc6d1a672dd4d06ceaf662
Vijay Benal <vbvijay84@gmail.com>
2026-07-04 20:56:19 +0530

    chore: increase main window width and update websocket URL to local backend

--- electron/main.ts
+++ electron/main.ts
@@ -9,7 +9,7 @@ let mainWindow: BrowserWindow | null = null;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 400,
+    width: 800,
     height: 600,
     alwaysOnTop: true,
     transparent: true,
